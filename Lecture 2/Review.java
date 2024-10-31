7c931f495ded4fb5adf8d47282d8cc2a59fea41b
Liam Shi <liamshi777@gmail.com>
2024-11-01 00:16:24 +1100

    Delete Lecture 2 directory

--- Lecture 2/Review.java
+++
@@ -1,9 +0,0 @@
-public class Review {
-    public static void main(String[] args) {
-        String a = "a";
-        String b = "letter b";
-        a = "letter a";
-        String c = a + " and " + b;
-        System.out.println(c);
-    }
-}
--- Lecture 2/Square3.java
+++
@@ -1,9 +0,0 @@
-public class Square3 {
-    public static void printSquare(double x) {
-        System.out.println(x*x);
-    }
-
-    public static void main(String[] args) {
-        printSquare(5);
-    }
-}
